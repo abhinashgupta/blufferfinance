175f64234a9006a05fcd1a5a6cfcb66aaa128b54
abhinashgupta <abhinahgupta1822@gmail.com>
2023-09-08 15:28:29 +0530

    first committt

--- src/components/Backed.js
+++ src/components/Backed.js
@@ -2,7 +2,7 @@ import "../css/Backed.css"
 import jumpLogo from "../images/backer-jump-logo.png"
 import arcanumLogo from "../images/backer-arcanum-logo.png"
 import iotexLogo from "../images/backer-iotex-logo.png"
-import ethLogo from "../images/eth-foundation.png"
+import ethLogo from "../images/images__2_-removebg-preview.png"
 
 const Backed = () => {
     return (
@@ -11,7 +11,7 @@ const Backed = () => {
           <h2 style={{marginTop:"5vw" , position:"relative" , top:"2vw" , fontSize:"4vw" , letterSpacing:".5vw" , fontFamily:"gilroy"}}>Funding</h2>
           <div className="grants-section">
             <a href="https://ethereum.org/en/">
-              <img height={70} style={{marginTop:"-1vw" , position:"relative" , top:"-1.5vw" , marginLeft:".8vw"}} src={ethLogo} />
+              <img height={100} style={{marginTop:"-1vw" , position:"relative" , top:"-1.5vw" , marginLeft:".8vw"}} src={ethLogo} />
             </a>
             <p style={{fontSize:"2vw"}}>
               Blufferfinance has received authorization for a grant from the
